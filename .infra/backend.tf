d26a334950b94ad62a2a869435e40a9cdcef00f4
nguyenknkn76 <nguyenknkn76@gmail.com>
2025-12-24 15:03:26 +0700

    deploy(git):state locking with dynamodb

--- .infra/backend.tf
+++ .infra/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "dop10-tfd-s3-an2-lenv-01"
     key = "dop10/dev-tfstate"
     region = "ap-northeast-2"
+    encrypt = true
+    dynamodb_table = "dop10-tfd-dynamodb-an2-lenv-01"
   }
 
   required_providers {
